4b6416aed888eb381855013c8bfbf4f6694784ca
Emily Nelson <evnelson2021@gmail.com>
2023-01-27 14:49:33 -0600

    pulling from main

---
+++ models/post.py
@@ -0,0 +1,13 @@
+class Post():
+    """Class that defines the properties for a post object"""
+
+    # Write the __init__ method here
+    def __init__(self, id, user_id, category_id, title, publication_date, image_url, content, approved):
+        self.id = id
+        self.user_id = user_id
+        self.category_id = category_id
+        self.title = title
+        self.publication_date = publication_date
+        self.image_url = image_url
+        self.content = content
+        self.approved = approved
